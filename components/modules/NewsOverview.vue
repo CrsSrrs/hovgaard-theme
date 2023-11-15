035ae49be3141d03f24ff6c252d194f0f2e7b124
Christoph Schroers <c.schroers@me-company.de>
2023-11-15 11:59:52 +0100

    add news overview switch, fix mobile gradient, fix border mobile nav

--- components/modules/NewsOverview.vue
+++ components/modules/NewsOverview.vue
@@ -1,5 +1,20 @@
 <template>
   <section class="news-overview" v-if="posts.length > 0">
+    <div class="row _switch">
+      <div class="col-24 col-xs-18 col-xs-offset-3 text-center">
+        <Button
+          v-for="{ url, title } in newsSwitch"
+          :key="url"
+          :href="url"
+          :icon="false"
+          class="-outline"
+          :class="{ '-active': isCurrent(url) }"
+          :disabled="isCurrent(url) || undefined"
+        >
+          {{ title }}
+        </Button>
+      </div>
+    </div>
     <div class="row">
       <div class="col-8 col-sm-12 col-xs-24" v-for="post of filteredPosts" :key="post.url">
         <NewsTeaser :data="post" :height="(itemHeight > minHeight) ? itemHeight : minHeight" @set-height="checkHeight"></NewsTeaser>
@@ -14,7 +29,7 @@
 </template>
 
 <script setup lang="ts">
-import { withDefaults, ref, computed, onMounted } from 'vue';
+import { withDefaults, ref, computed } from 'vue';
 import { usePosts } from '@/composables/usePosts';
 
 import Button from '@/components/atoms/Button.vue';
@@ -35,11 +50,30 @@ const filteredPosts = computed(() => {
   return posts.slice(0, count.value);
 });
 
+const newsSwitch = [
+  {
+    url: 'https://www.bigambitionsgame.com/news',
+    title: 'Big Ambitions',
+  },
+  {
+    url: 'https://www.startupcompanygame.com/news',
+    title: 'Startup Company',
+  },
+  {
+    url: 'https://www.hovgaard.com/news',
+    title: 'Hovgaard Games',
+  }
+];
+
 const minHeight = 250;
 let itemHeight = ref(0);
 
 function checkHeight(value) {
   itemHeight.value = (itemHeight.value > value) ? itemHeight.value : value;
+}
+
+function isCurrent(url) {
+  return url.includes(window.location.host);
 }
 </script>
 
